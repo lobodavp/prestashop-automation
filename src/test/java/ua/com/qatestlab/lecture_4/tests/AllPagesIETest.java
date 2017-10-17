97fe165dffc3a2397cd5402d8c14aa72da4f30fb
Michael Loboda <lobodavp@gmail.com>
2017-10-17 14:20:34 +0300

    instanses of browsers in the AllPagesFirefoxTest and  AllPagesIETest were changed to correct ones

--- src/test/java/ua/com/qatestlab/lecture_4/tests/AllPagesIETest.java
+++ src/test/java/ua/com/qatestlab/lecture_4/tests/AllPagesIETest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -32,7 +33,7 @@ public class AllPagesIETest {
         System.setProperty(IEProp,
                 new File(AllPagesIETest.class.getResource(IEFile).getFile()).getPath());
         Reporter.log("Creating browser instance <br />");
-        driver = new ChromeDriver();
+        driver = new InternetExplorerDriver();
         Reporter.log("Setup implicitlyWait<br />");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         Reporter.log("Maximaze browser window <br />");
